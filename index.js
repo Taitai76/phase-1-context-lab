2433649ce9188a49b57b1482d8c577e2153ee071
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-06-24 19:06:51 -0400

    commit tree f23374e53259c0a544dd867bf8148be7a2afbb51

--- index.js
+++ index.js
@@ -1,4 +1,91 @@
 /* Your Code Here */
+function createEmployeeRecord(info){
+    const Employee = {
+        firstName: `${info[0]}`,
+        familyName: `${info[1]}`,
+        title: `${info[2]}`,
+        payPerHour: info[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+    return Employee;
+}
+
+function createEmployeeRecords(info){
+    let newArray = [];
+    for (const a of info){
+        newArray.push(createEmployeeRecord(a)) 
+    }
+    return newArray;
+}
+
+function createTimeInEvent(date){
+    const modDate = date.split(" ")
+    let hours = parseInt(modDate[1])
+    const newObj={
+        type: "TimeIn",
+        date: modDate[0],
+        hour: hours
+    };
+    this.timeInEvents.push(newObj);
+    return this;
+}
+
+function createTimeOutEvent(date){
+    const modDate= date.split(" ")
+    let hours = parseInt(modDate[1])
+    const newObj={
+        type: "TimeOut",
+        date: modDate[0],
+        hour: hours
+    };
+    this.timeOutEvents.push(newObj);
+    return this;
+}
+function hoursWorkedOnDate(dateS){
+    let inTime= 0;
+    let outTime= 0;
+    for(const A of this.timeInEvents){
+        if (A['date'] === dateS){
+            inTime = A['hour'];
+        }
+    }
+    for(const A of this.timeOutEvents){
+        if (A['date'] === dateS){
+            outTime = A['hour'];
+        }
+    }
+    return (outTime-inTime)/100;
+}
+
+function wagesEarnedOnDate(date){
+    let hours = hoursWorkedOnDate.call(this, date);
+    return (hours*this.payPerHour)
+}
+
+function findEmployeeByFirstName(Employees, name){
+    for(const A of Employees){
+        if (A.firstName === name){
+            return A;
+        }
+    }
+
+}
+
+function calculatePayroll(Employees){
+    let wages = []
+
+    for(const A of Employees){
+        let x = allWagesFor.apply(A);
+        wages.push(x)
+    }
+    console.log(wages);
+    const total = wages.reduce((n, c)=>{
+        return n + c;
+    }, 0);
+    return total;
+
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +107,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
